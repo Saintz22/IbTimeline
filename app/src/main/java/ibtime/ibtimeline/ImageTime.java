3d497f3479124d3cec95e642a1ea49a77e70d9e0
Saintz22 <nickwilsmore@gmail.com>
2017-03-20 20:39:41 +0000

    Base files

---
+++ app/src/main/java/ibtime/ibtimeline/ImageTime.java
@@ -0,0 +1,18 @@
+package ibtime.ibtimeline;
+
+/**
+ * Created by Nicholas on 20/03/2017.
+ */
+
+public class ImageTime {
+
+    public static void forwardTime()
+    {
+
+    }
+
+    public static void backTime()
+    {
+
+    }
+}
